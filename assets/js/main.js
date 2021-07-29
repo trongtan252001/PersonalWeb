6860462166042b4bc6d53029809f49645a76705a
trong <trongtan252001@gmail.com>
2021-07-29 16:04:58 +0700

    first comit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+// menu show y hidden
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav__link");
+navLink.forEach((n) =>
+  n.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  })
+);
