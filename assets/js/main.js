ec882f6d601248e76670292dd92b56e19215f12b
trong <trongtan252001@gmail.com>
2021-07-31 23:54:18 +0700

    feature: Accordion skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,32 @@ navLink.forEach((n) =>
     navMenu.classList.remove("show-menu");
   })
 );
+/*==================== Accordion skills c1 ====================*/
+const skillsHeader = document.querySelectorAll(".skills__header");
+const skillsContent = document.querySelectorAll(".skills__content");
+skillsHeader.forEach((n) => {
+  n.addEventListener("click", () => {
+    for (i = 0; i < skillsContent.length; i++) {
+      skillsContent[i].className = "skills__content skills__close";
+    }
+    if (n.parentNode.className === "skills__content skills__close") {
+      n.parentNode.className = "skills__content skills__open";
+    }
+  });
+});
+/*==================== Accordion skills  c2====================*/
+
+// const skillsContent = document.getElementsByClassName("skills__content");
+// const skillsHeader = document.querySelectorAll(".skills__header");
+// function toggleSkills() {
+//   let itemClass = this.parentNode.className;
+//   for (i = 0; i < skillsContent.length; i++) {
+//     skillsContent[i].className = "skills__content skills__close";
+//   }
+//   if (itemClass === "skills__content skills__close") {
+//     this.parentNode.className = "skills__content skills__open";
+//   }
+// }
+// skillsHeader.forEach((el) => {
+//   el.addEventListener("click", toggleSkills);
+// });
